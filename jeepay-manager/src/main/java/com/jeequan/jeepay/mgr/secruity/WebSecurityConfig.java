548e3fc706957a81b826c420ccdc676b3c4dad63
Seventeen-117 <yuanyangqing@icloud.com>
2025-04-19 14:12:39 +0800

    0419

--- jeepay-manager/src/main/java/com/jeequan/jeepay/mgr/secruity/WebSecurityConfig.java
+++ jeepay-manager/src/main/java/com/jeequan/jeepay/mgr/secruity/WebSecurityConfig.java
@@ -161,8 +161,9 @@ public class WebSecurityConfig{
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         if(systemYmlConfig.getAllowCors()){
             CorsConfiguration config = new CorsConfiguration();
-            config.setAllowCredentials(true);
-            config.addAllowedOrigin(CorsConfiguration.ALL);  //允许跨域的域名， *表示允许任何域名使用
+            config.setAllowCredentials(true);   //带上cookie信息
+            // 不能同时使用 allowCredentials=true 和 allowedOrigins="*"
+            // config.addAllowedOrigin(CorsConfiguration.ALL);  //允许跨域的域名， *表示允许任何域名使用
             config.addAllowedOriginPattern(CorsConfiguration.ALL);  //使用addAllowedOriginPattern 避免出现 When allowCredentials is true, allowedOrigins cannot contain the special value "*" since that cannot be set on the "Access-Control-Allow-Origin" response header. To allow credentials to a set of origins, list them explicitly or consider using "allowedOriginPatterns" instead.
             config.addAllowedHeader(CorsConfiguration.ALL);   //允许任何请求头
             config.addAllowedMethod(CorsConfiguration.ALL);   //允许任何方法（post、get等）
